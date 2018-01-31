003a154f7beecea42df683f1766cf8fcac2cee1c
dbfarms <matt.hunger@gmail.com>
2018-01-31 13:13:03 +0000

    Done.

--- src/containers/PetsPage.js
+++ src/containers/PetsPage.js
@@ -13,13 +13,26 @@ class PetsPage extends Component {
   }
 
   render() {
+    const { match, pets } = this.props;
+    //debugger 
+  
     return (
-      <div>Pets Page</div>
+      <div>
+        <PetsList pets={pets} />
+        <Switch>
+          <Route path={`${match.url}/new`} component = {PetsNew} />
+          <Route path={`${match.url}/:petId`} component={PetsShow} />
+          <Route exact path={match.url} render={() => (
+            <h3>Please select a Pet from the list.</h3>
+          )} />
+        </Switch>
+      </div>
     )
   }
 };
 
 const mapStateToProps = state => {
+  //debugger
   return {
     pets: state.pets
   };
